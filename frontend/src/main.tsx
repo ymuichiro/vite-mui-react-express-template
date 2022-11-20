ab4dc7096f5c32a2b0d6db83b1b0d8afdb39ff83
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2022-11-20 09:46:14 +0900

    [add] change react-helmet-async from react-helmet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,20 +6,22 @@ import CssBaseline from '@/components/atom/CssBaseline';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { theme } from './theme';
 import { RecoilRoot } from 'recoil';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Helmet>
-      <link rel='icon' type='image/svg+xml' href='/vite.svg' />
-      <title>Vite + React + TS</title>
-    </Helmet>
-    <ThemeProvider theme={theme}>
-      <RecoilRoot>
-        <CssBaseline>
-          <Root />
-        </CssBaseline>
-      </RecoilRoot>
-    </ThemeProvider>
+    <HelmetProvider>
+      <Helmet>
+        <link rel='icon' type='image/svg+xml' href='/vite.svg' />
+        <title>Vite + React + TS</title>
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <CssBaseline>
+            <Root />
+          </CssBaseline>
+        </RecoilRoot>
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
